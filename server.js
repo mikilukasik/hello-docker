7a4cc5616ba318d85338a7c784ff1e105c4a3e88
mikiLukasik <miklosbacsi@gmail.com>
2016-10-09 18:41:55 +0100

    .

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res){
-  res.json( process.env )
+  res.send( process.env.NODE_ENV )
   //res.send('Hello world from Distelli & Docker!');
 });
 
